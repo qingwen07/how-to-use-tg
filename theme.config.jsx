072c547119be99b0f8131744e02b9a82906f9294
xqw <xiongqingwen@mycs.cn>
2024-11-25 19:39:21 +0800

    增加谷歌分析,google adsense

--- theme.config.jsx
+++ theme.config.jsx
@@ -7,9 +7,9 @@ const SEO_KEYWORDS = [
 export default {
   logo: <span>How to Use Telegram</span>,
   project: {
-    link: 'https://github.com/qingwen07/how-to-use-telegram'
+    link: 'https://github.com/qingwen07/how-to-use-tg'
   },
-  docsRepositoryBase: 'https://github.com/qingwen07/how-to-use-telegram',
+  docsRepositoryBase: 'https://github.com/qingwen07/how-to-use-tg',
   footer: {
     text: '© 2024 How to Use Telegram Guide.'
   },
@@ -23,6 +23,24 @@ export default {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="keywords" content={SEO_KEYWORDS} /> 
       <meta name="description" content="A comprehensive guide on how to use Telegram" />
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
+      {/* Google Analytics */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-HC47H0TN39"
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HC47H0TN39');
+          `,
+        }}
+      />
+      
     </>
   )
 }
