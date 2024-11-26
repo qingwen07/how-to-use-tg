f39a1a5e00ec1880f7424e54aca8e1831c18c934
xqw <xiongqingwen@mycs.cn>
2024-11-26 10:12:55 +0800

    add how-to-use-telegram-bots.mdx

--- pages/zh/_meta.js
+++ pages/zh/_meta.js
@@ -6,5 +6,6 @@ export default {
   security: "安全与隐私",
   tips: "使用技巧",
   "telegram-without-phone": "如何不用手机号码使用 Telegram",
-  "how-to-find-telegram-channels": "如何查找 Telegram 频道"
+  "how-to-find-telegram-channels": "如何查找 Telegram 频道",
+  "how-to-use-telegram-bots": "如何使用 Telegram 机器人"
 }
